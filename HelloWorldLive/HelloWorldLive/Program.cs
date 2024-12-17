3498e05035518e6d7f0455a804d0f88cc2f5a407
sertanbozkus <sertan.bozkus@bilgeadam.com>
2024-12-17 21:08:38 +0300

    Add files via upload

---
+++ HelloWorldLive/HelloWorldLive/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Bugün 17 Aralık 2024 ve ben yeni sınıfıma ilk bu projeyi yapıyorum.
+
+//Burası yorum satırı olacak.
+
+//dwadawd
+//    dwadwad
+//    dwad
+//    dwad
+
+
+Console.WriteLine("Bu kısmın içerisi metindir.");
+Console.WriteLine("Bu benim ilk c# uygulamam.");
+
+Console.WriteLine("------------------------------");
+
+Console.Write("Bu kısmın içerisi metindir.");
+Console.Write("Bu benim ilk c# uygulamam.");
+
+Console.WriteLine("\n-------------------------------");
+
+Console.WriteLine("Bu ilk satır.\nBu ikinci satır.");
+
+Console.WriteLine("-----------------------------");
+
+Console.ReadLine();
---
+++ HelloWorldLive/VariablesLive/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+string isim1;
+isim1 = "sertan";  // Assignment -> atama
+                   // = -> atama operatörü
+
+int yas = 28;
+
+Console.WriteLine("Commitlenmiş hali pushluyorum.");
+
+/*
+
+string isim1;
+int yas;
+
+isim1="sertan";
+yas = 28;
+
+
+*/
